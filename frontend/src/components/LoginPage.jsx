6b670168315d393d153b2d4eb583c4a9e2cdf0e8
Deyi <z5464292@ad.unsw.edu.au>
2024-11-11 22:59:04 +1100

    Add the Login page and change a little Landing page

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const LeftSection = styled.div`
+  flex: 1.5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
+  text-align: center;
+  background-image: url('https://images.pexels.com/photos/6476254/pexels-photo-6476254.jpeg?auto=compress&cs=tinysrgb&w=1200');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const RightSection = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding:20px;
+  background-color: #ffffff;
+`;
+
+const TextBackground = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  border-radius: 5px;
+  width: 80%;
+`;
+
+const Heading = styled.h1`
+  font-size: 4rem;
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.5);
+`;
+
+const Text = styled.p`
+  font-size: 1.4rem;
+  color: white;
+`;
+
+const H2 = styled.h2`
+  font-size: 2.5rem;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 80%;
+  padding: 12px;
+  margin-left: 30px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+
+  &:focus {
+    border-color: #4DB6AC; // Focus state color
+  }
+`;
+
+const Button = styled.button`
+  width: 40%;
+  padding: 15px;
+  margin-left: 30px;
+  font-size: 18px;
+  color: white;
+  background-color: #4DB6AC;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #35a79c;
+  }
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-weight: bold;
+`;
+
+
+function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:5005/admin/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                })
+            });
+            const data = await response.json();
+            if (response.ok) {
+                localStorage.setItem('token', data.token)
+                console.log(data.token)
+                navigate('/dashboard');
+            } else {
+                setError(data.error || 'Invalid email or password');
+            }
+        } catch (err) {
+            setError('An error occurred. Please try again.');
+        }
+    };
+
+    const handleBack = () => {
+        navigate('/');
+    };
+
+    return (
+        <StyleDiv>
+            <LeftSection>
+                <TextBackground>
+                    <Heading>Welcome to Presto!</Heading>
+                    <Text>Lighter, more enjoyable, more funny for making presentations</Text>
+                </TextBackground>
+            </LeftSection>
+            <RightSection>
+                <H2>Login Form</H2>
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <Input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    {error && <ErrorMessage>{error}</ErrorMessage>}
+                    <Button type="submit">Login</Button>
+                    <Button onClick={handleBack} type="button">Back</Button>
+                </form>
+            </RightSection>
+        </StyleDiv >
+    );
+}
+
+export default LoginPage;

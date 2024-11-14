4dcd26968c4900e0a18d3742b805ab72388536ab
Deyi <z5464292@ad.unsw.edu.au>
2024-11-15 04:53:46 +1100

    I lint the code and found 1293 error, and after fixing i found i almost need to re-upload the code

--- frontend/src/components/CheckToken.jsx
+++ frontend/src/components/CheckToken.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function TokenCheckRoute({ children }) {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-    if (!token) {
-        return <Navigate to="/login" />;
-    }
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
 
-    return children;
+  return children;
 }
 
 export default TokenCheckRoute;
